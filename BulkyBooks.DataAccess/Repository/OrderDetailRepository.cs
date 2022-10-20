d7d33452bb7ec748fdb2f8987e374ac6e3dc90b7
bartosz0 <methkill@gmail.com>
2022-10-20 10:51:07 +0200

    added Order Header and Detail to UnitOfWork

---
+++ BulkyBooks.DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,21 @@
+﻿using BulkyBooks.DataAccess.Repository.IRepository;
+using BulkyBooks.Models;
+
+namespace BulkyBooks.DataAccess.Repository
+{
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        private ApplicationDbContext _db;
+
+        public OrderDetailRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+        public void Update(OrderDetail obj)
+        {
+            _db.OrderDetail.Update(obj);
+        }
+    }
+}
